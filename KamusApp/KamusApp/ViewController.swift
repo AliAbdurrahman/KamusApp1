48695954f7059b16d963b62788be72d698c154c1
Ali Abdurrahman <aliabdurrahman25032001@gmail.com>
2017-11-10 07:04:12 +0700

    fix

---
+++ KamusApp/KamusApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KamusApp
+//
+//  Created by Ali Abdurrahman Hidayat on 03/11/17.
+//  Copyright © 2017 Ali Abdurrahman Hidayat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
